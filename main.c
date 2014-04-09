b6078082b51b7ab00ec089022054f94b228c9904
João Júnior <joaojunior_ma@gmail.com>
2014-04-08 21:30:50 -0300

    Refatoração para trocar nomes de variáveis

--- main.c
+++ main.c
@@ -8,15 +8,15 @@ int *calculate_communities_floyd_warshall(Graph *graph, int quantity_communities
 };
 
 int *calculate_communities(Graph *graph, int quantity_communities, RepeatedSquaringResult *(*func)(Graph *graph)){
-    ResultShortestPath *repeated_squaring_result;
+    ResultShortestPath *shortest_path_result;
     int **quantity_shortest_path_in_edge;
     int *communities2nodes;
     int numbers_communities;
     Arc *arc;
     do{
-        repeated_squaring_result = (ResultShortestPath *)(*func)(graph);
-        quantity_shortest_path_in_edge = calculate_quantity_shortest_path_in_edges(repeated_squaring_result->predecessor, graph->numbers_nodes);
-        communities2nodes = calculate_communities(repeated_squaring_result->distance, graph->numbers_nodes);
+        shortest_path_result = (ResultShortestPath *)(*func)(graph);
+        quantity_shortest_path_in_edge = calculate_quantity_shortest_path_in_edges(shortest_path_result->predecessor, graph->numbers_nodes);
+        communities2nodes = calculate_communities(shortest_path_result->distance, graph->numbers_nodes);
         numbers_communities = get_max_community(communities2nodes, graph->numbers_nodes);
         if(numbers_communities < quantity_communities){
             arc = get_arc_with_bigger_weight(quantity_shortest_path_in_edge, graph->numbers_nodes);
