ff0a6903557ec5fdbcf05dfc846214e5a97b8e80
João Júnior <joaojunior_ma@gmail.com>
2014-04-11 16:12:15 -0300

    Adicionantes testes no cálculo de comunidades com o algoritmo de Johnson com Fila de Prioridade

--- tests/search_community_with_johnson_queue_unittest.cc
+++ tests/search_community_with_johnson_queue_unittest.cc
@@ -5,7 +5,7 @@
  
 TEST(SearchComunityTest, VerifyQuantityShortestPathInEdgeWithJohnsonQueue){
     Graph graph;
-    JohnsonResult *floyd_warshall_result;
+    JohnsonResult *johnson_result;
     int **quantity_shortest_path_in_edge;
     graph.numbers_nodes = 5;
     allocate_memory(&graph);
@@ -18,8 +18,8 @@ TEST(SearchComunityTest, VerifyQuantityShortestPathInEdgeWithJohnsonQueue){
     insert_arc(&graph, 3, 2, -5);
     insert_arc(&graph, 3, 0, 2);
     insert_arc(&graph, 4, 3, 6);
-    floyd_warshall_result = johnson_queue(&graph);
-    quantity_shortest_path_in_edge = calculate_quantity_shortest_path_in_edges(floyd_warshall_result->predecessor, graph.numbers_nodes);
+    johnson_result = johnson_queue(&graph);
+    quantity_shortest_path_in_edge = calculate_quantity_shortest_path_in_edges(johnson_result->predecessor, graph.numbers_nodes);
     ASSERT_EQ(0,quantity_shortest_path_in_edge[0][0]);
     ASSERT_EQ(0,quantity_shortest_path_in_edge[0][1]);
     ASSERT_EQ(0,quantity_shortest_path_in_edge[0][2]);
@@ -49,7 +49,7 @@ TEST(SearchComunityTest, VerifyQuantityShortestPathInEdgeWithJohnsonQueue){
 
 TEST(SearchComunityTest, VerifyQuantityShortestPathInEdgeWithJohnsonQueue2){
     Graph graph;
-    JohnsonResult *floyd_warshall_result;
+    JohnsonResult *johnson_result;
     int **quantity_shortest_path_in_edge;
     graph.numbers_nodes = 4;
     allocate_memory(&graph);
@@ -58,8 +58,8 @@ TEST(SearchComunityTest, VerifyQuantityShortestPathInEdgeWithJohnsonQueue2){
     insert_edge(&graph, 0, 2, 1);
     insert_edge(&graph, 1, 2, 1);
     insert_edge(&graph, 2, 3, 1);
-    floyd_warshall_result = johnson_queue(&graph);
-    quantity_shortest_path_in_edge = calculate_quantity_shortest_path_in_edges(floyd_warshall_result->predecessor, graph.numbers_nodes);
+    johnson_result = johnson_queue(&graph);
+    quantity_shortest_path_in_edge = calculate_quantity_shortest_path_in_edges(johnson_result->predecessor, graph.numbers_nodes);
     ASSERT_EQ(0,quantity_shortest_path_in_edge[0][0]);
     ASSERT_EQ(1,quantity_shortest_path_in_edge[0][1]);
     ASSERT_EQ(2,quantity_shortest_path_in_edge[0][2]);
@@ -78,7 +78,7 @@ TEST(SearchComunityTest, VerifyQuantityShortestPathInEdgeWithJohnsonQueue2){
     ASSERT_EQ(0,quantity_shortest_path_in_edge[3][3]);
 }
 
-TEST(SearchComunityTest, CalculateCommunitiesWithJohnsonQueueWith1Community){
+TEST(SearchComunityTest, CalculateCommunitiesWith1Community){
     Graph graph;
     int *communities2nodes;
     graph.numbers_nodes = 4;
@@ -92,6 +92,54 @@ TEST(SearchComunityTest, CalculateCommunitiesWithJohnsonQueueWith1Community){
     ASSERT_EQ(1, communities2nodes[1]);
     ASSERT_EQ(1, communities2nodes[2]);
     ASSERT_EQ(1, communities2nodes[3]);
+}
+
+TEST(SearchComunityTest, CalculateCommunitiesWith2Communities){
+    Graph graph;
+    int *communities2nodes;
+    graph.numbers_nodes = 4;
+    allocate_memory(&graph);
+    insert_edge(&graph, 0, 1, 1);
+    insert_edge(&graph, 0, 2, 1);
+    insert_edge(&graph, 1, 2, 1);
+    insert_edge(&graph, 2, 3, 1);
+    communities2nodes = calculate_communities_johnson_queue(&graph, 2);
+    ASSERT_EQ(1, communities2nodes[0]);
+    ASSERT_EQ(1, communities2nodes[1]);
+    ASSERT_EQ(1, communities2nodes[2]);
+    ASSERT_EQ(2, communities2nodes[3]);
+}
+
+TEST(SearchComunityTest, CalculateCommunitiesWith3Communities){
+    Graph graph;
+    int *communities2nodes;
+    graph.numbers_nodes = 4;
+    allocate_memory(&graph);
+    insert_edge(&graph, 0, 1, 1);
+    insert_edge(&graph, 0, 2, 1);
+    insert_edge(&graph, 1, 2, 1);
+    insert_edge(&graph, 2, 3, 1);
+    communities2nodes = calculate_communities_johnson_queue(&graph, 3);
+    ASSERT_EQ(1, communities2nodes[0]);
+    ASSERT_EQ(2, communities2nodes[1]);
+    ASSERT_EQ(2, communities2nodes[2]);
+    ASSERT_EQ(3, communities2nodes[3]);
+}
+
+TEST(SearchComunityTest, CalculateCommunitiesWith4Communities){
+    Graph graph;
+    int *communities2nodes;
+    graph.numbers_nodes = 4;
+    allocate_memory(&graph);
+    insert_edge(&graph, 0, 1, 1);
+    insert_edge(&graph, 0, 2, 1);
+    insert_edge(&graph, 1, 2, 1);
+    insert_edge(&graph, 2, 3, 1);
+    communities2nodes = calculate_communities_johnson_queue(&graph, 4);
+    ASSERT_EQ(1, communities2nodes[0]);
+    ASSERT_EQ(2, communities2nodes[1]);
+    ASSERT_EQ(3, communities2nodes[2]);
+    ASSERT_EQ(4, communities2nodes[3]);
 }
 
 int main(int argc, char **argv) {
