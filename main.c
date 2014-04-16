d5d275a8992eaf040e5da4c87864aebd7b4f0232
João Júnior <joaojunior_ma@gmail.com>
2014-04-16 12:08:21 -0300

    refatorando a leitura dos arquivos de entrada

--- main.c
+++ main.c
@@ -40,26 +40,44 @@ int *calculate_communities(Graph *graph, int quantity_communities, RepeatedSquar
 };
 
 int main(int argc, char *argv[]){
-    int source, dest;
+    int source, dest, min_node, max_node;
     int *communities2nodes;
+    Queue nodes;
     Graph graph;
     FILE *arq;
     Func v[5] = {calculate_communities_repeated_square, calculate_communities_floyd_warshall, calculate_communities_johnson_queue, 
                 calculate_communities_johnson_array, calculate_communities_nbfs};
-    graph.numbers_nodes = 12;
-    allocate_memory(&graph);
+    min_node = MAX_WEIGHT;
+    max_node = -1;
     if(argc == 3){
         arq = fopen(argv[1], "r");
         if(arq == NULL)
                 printf("Erro, nao foi possivel abrir o arquivo\n");
         else
-            while((fscanf(arq,"%d %d\n", &source, &dest)) !=EOF)
-                insert_edge(&graph, source-1, dest-1, 1);
+            while((fscanf(arq,"%d %d\n", &source, &dest)) !=EOF){
+                if(source < min_node)
+                    min_node = source;
+                if(dest < min_node)
+                    min_node = dest;
+                if(source > max_node)
+                    max_node = source;
+                if(dest > max_node)
+                    max_node = dest;
+                enqueue(&nodes, source);
+                enqueue(&nodes, dest);
+            }
                 //printf("%d %d\n", source, dest);
         fclose(arq);
+        graph.numbers_nodes = max_node - min_node + 1;
+        allocate_memory(&graph);
+        while(not empty(&nodes)){
+            source = dequeue(&nodes);
+            dest = dequeue(&nodes);
+            insert_edge(&graph, source - min_node, dest - min_node, 1);
+        }
         communities2nodes = v[atoi(argv[2])](&graph, 4);
         for(int i = 0; i < graph.numbers_nodes; i++)
-            printf("%d %d\n", i+1, communities2nodes[i]);
+            printf("%d %d\n", i + min_node, communities2nodes[i]);
     }
     return 0;
 }
